e6d0c00cf5aa8145e8fe3b4b95693f089a3d65f7
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-02-28 15:43:37 +0530

    Create AREA OF FARM.C
    TO FIND AREA OF FARM

---
+++ AREA OF FARM.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+	float l,b,a;
+	printf("enter the length and breadth values of the farm:");
+	scanf("%f %f",&l,&b);
+	a=l*b;
+	printf("\n%0.5f",a);
+	return 0;
+}
